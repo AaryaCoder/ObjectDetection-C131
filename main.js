8c26d775f5b7bb877e4c5a80309cd57d6795d698
AaryaCoder <84276676+AaryaCoder@users.noreply.github.com>
2022-05-17 18:25:47 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 img="";
 status="";
+objects=[];
 
 function preload(){
     img=loadImage('dog_cat.jpg');
@@ -11,26 +12,29 @@ function setup(){
 }
 function draw(){
     image(img,0,0,640,420);
-    fill("#018891");
-    text("Dog",45,75);
-    noFill();
-    stroke("#018891");
-    rect(30,60,450,350);
+    if(status!=""){
+        for(i=0;1<objects.length;i++){
+            document.getElementById("status").innerHTML="Status: Object Detected :)";
 
-    fill("#fc7560");
-    text("Cat",320,120);
-    noFill();
-    stroke("#fc7560");
-    rect(300,90,270,320);
+            fill("#f76d52");
+            percent= floor(objects[i].confidence*100);
+            text(objects[i].label + "" + percent + "%", objects[i].x, objects[i].y);
+            noFill();
+            stroke("#f76d52");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+    
 }
 function modelLoaded(){
     console.log("Model Loaded!");
     status=true;
     objectDetector.detect(img,gotResult);
 }
-function gotResult(error){
+function gotResult(error,results){
     if(error){
         console.log(error);
     }
     console.log(results);
+    objects=results;
 }
